443a5c91a232391141503647cdd20a331c8ef335
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 11:51:16 -0300

    adicionando arquivos

---
+++ manipulando-texto.js
@@ -0,0 +1,11 @@
+let txt = "Estão chegando as provas";
+let numfloat = 123.543;
+let linguagens = ["Java","C#","Python"];
+                    //0    1      2
+
+//console.log(typeof numfloat.toString());
+console.log(typeof String(numfloat));
+
+console.log(typeof linguagens[0]);
+
+console.log(txt.replace("povas", "avaiações"));
---
+++ tipos de dados .js
@@ -0,0 +1,17 @@
+// números inteiros
+let idadeAluno = 18;
+
+// Número flutuantes 
+let valorProduto=5;
+
+//string
+let nomeAluno = "Pedro Henrique";
+
+//Boolean
+let casado = true
+
+//console.log(typeof idadeAluno);
+//console.log(typeof valorProduto);
+//console.log(typeof nomeAluno);
+console.log(typeof casado);
+
