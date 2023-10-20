a806625f1b6bc30149e9efc7902870a863f51aff
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 10:42:31 -0300

    adicionando arquivo de incremento e decremento

---
+++ decremento.js
@@ -0,0 +1,19 @@
+let valor = 7;
+
+console.log(valor);
+
+valor --;
+
+console.log(valor);
+
+valor -= 2;
+
+console.log(valor);
+
+valor *= 4;
+
+console.log(valor)
+
+valor  /=4;
+
+console.log(valor)
---
+++ incremento.js
@@ -0,0 +1,9 @@
+let valor = 7;
+
+cosole.console.log(valor)  //valor igaul a 7
+valor = valor + 1;
+console.log(valor);  //valor igual a 8
+
+valor ++;
+
+console.log(valor)
