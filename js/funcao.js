a4f018355f4b3f3ab3ca0bee7ccc7ddc1b864e29
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 10:53:05 -0300

    funções

---
+++ js/funcao.js
@@ -0,0 +1,21 @@
+function teste (){
+    console.log("Oláaaaa");
+}
+
+function meuNome(name){
+    console.log(`Olá ${name}`);
+}
+
+function calc(arg, arg2){
+    return arg + arg2;
+}
+
+
+console.log(calc(4, 4));
+console.log(calc(5, 4));
+console.log(calc(10, 10));
+
+
+
+
+teste();
